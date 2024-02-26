71208e373ac95ca87313e597887794b631cad6bc
wajahat16079 <termazie@hotmail.com>
2024-02-25 21:56:57 -0500

    link updated

--- app.R
+++ app.R
@@ -13,9 +13,10 @@ server = function(input, output) {
     # temp = "tiles/{z}/{x}/{-y}.png"
     # temp = "WDPA_50_tiles/{z}/{x}/{-y}.png"
     # temp = "wpda50/{z}/{x}/{-y}.png"
-    temp = "Underprotected/{z}/{x}/{-y}.png"
-    # temp = "https://wajahat16079.github.io/checkRastersTiles/www/Underprotected/{z}/{x}/{y}.png"
-    
+    # temp = "Underprotected/{z}/{x}/{-y}.png"
+    # temp = "https://wajahat16079.github.io/checkRastersTiles/www/wpda50/{z}/{x}/{y}.png"
+    # temp = "https://github.com/wajahat16079/checkRastersTiles/www/wpda50/{z}/{x}/{y}.png"
+    temp = "https://raw.githubusercontent.com/wajahat16079/checkRastersTiles/main/www/wpda50/{z}/{x}/{-y}.png"
     # an fans 
     leaflet() %>%
       addProviderTiles(providers$Esri.WorldTopoMap, group = "base") %>%
